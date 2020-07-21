3a44554c703c660751f13c3c55ee09b15af49585
Anthony Terrell <anthonygterrell@gmail.com>
2020-07-21 08:58:57 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,10 @@
+
+### Accessing your Cultrured Code Things Sqlite Database
+[Article](https://culturedcode.com/things/support/articles/2982272/)
+```
+~/Library/Containers/com.culturedcode.ThingsMac/Data/Library/Application Support/Cultured Code/Things/
+```
+Using the absolute path
+```
+/Users/your-user/Library/Containers/com.culturedcode.ThingsMac/Data/Library/Application Support/Cultured Code/Things/Things.Sqlite3
+```
---
+++ app/Task.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Task extends Model
+{
+    protected $table = 'TMTask';
+
+    public function scopeCancelled($query)
+    {
+        return $query->where('status', 2);
+    }
+
+    public function scopeCompleted($query)
+    {
+        return $query->where('status', 3);
+    }
+
+    public function scopeIncomplete($query)
+    {
+        return $query->where('status', 0);
+    }
+
+    public function scopeScheduled($query)
+    {
+        return $query->where('todayIndex', '>', 0);
+    }
+
+    public function scopeTrashed($query)
+    {
+        return $query->where('trashed', 1);
+    }
+}
